43b7e57eca5203bd3b7e431b907724377840ba18
Evgeniy Allenov <imr@inbox.ru>
2016-03-28 15:12:24 +0300

    Fixed eslinter + error handling without breaking watche

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,24 @@ const gulp = require('gulp'),
     del = require('del'),
     concat = require('gulp-concat'),
     babel = require('gulp-babel'),
+    eslint = require('gulp-eslint'),
     less = require('gulp-less'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     uglify = require('gulp-uglify'),
     maps = require('gulp-sourcemaps'),
     browserSync = require('browser-sync').create()
-
 const paths = {
     src: './src',
     build: './build',
     node: './node_modules'
 }
-
-const jsLibraries = [
-    `${paths.node}/jquery/dist/jquery.js`
-]
-
-const jsApp = [
-    `${paths.src}/js/app.js`
-]
+const js = {
+    app: `${paths.src}/js/app.js`,
+    libraries: `${paths.node}/jquery/dist/jquery.js`
+}
 // ----------------- //
+
 
 //CLEAN
 gulp.task('clean', function () {
@@ -34,12 +31,17 @@ gulp.task('clean', function () {
 })
 // ----------------- //
 
+
 // LESS COMPILERS && AUTOPREFIXING
 gulp.task('styles', function() {
     return gulp
             .src(`${paths.src}/LESS/master.less`)
             .pipe(maps.init())
             .pipe(less())
+            .on('error', (err) => {
+                console.log(`Plugin: ${err.plugin}`)
+                console.log(`Error: ${err.message}`)
+            })
             .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 10'))
             .pipe(cleanCSS())
             .pipe(maps.write('./'))
@@ -53,41 +55,52 @@ gulp.task('styles', function() {
 gulp.task('html', function () {
     return gulp.src(`${paths.src}/**/*.html`).pipe(gulp.dest(paths.build))
 })
-
 gulp.task('img', function () {
     return gulp.src(`${paths.src}/img/*`).pipe(gulp.dest(`${paths.build}/img`))
 })
-
 gulp.task('fonts', function () {
     return gulp.src(`${paths.src}/fonts/*`).pipe(gulp.dest(`${paths.build}/fonts`))
 })
-
 gulp.task('static', function () {
     return gulp.start(['html', 'img', 'fonts'])
 })
 // ----------------- //
 
+
 // JS COMPILE
+gulp.task('lint', function () {
+    return gulp.src(js.app)
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.results(function (results) {
+                console.log('LINTER RESULTS:');
+                console.log('Total Warnings: ' + results.warningCount);
+                console.log('Total Errors: ' + results.errorCount);
+            }))
+})
 gulp.task('js-libraries', function () {
-    return gulp.src(jsLibraries)
+    return gulp.src(js.libraries)
             .pipe(concat('libraries.js'))
             .pipe(uglify())
             .pipe(gulp.dest(`${paths.build}/js`))
 })
-
-gulp.task('js-app', function () {
-    return gulp.src(jsApp)
+gulp.task('js-app', ['lint'], function () {
+    return gulp.src(js.app)
             .pipe(maps.init())
             .pipe(babel({presets: ['es2015']}))
+            .on('error', (err) => {
+                console.log(`Plugin: ${err.plugin}`)
+                console.log(`Error: ${err.message}`)
+            })
             .pipe(concat('app.js'))
             .pipe(maps.write('./'))
             .pipe(gulp.dest(`${paths.build}/js/`))
 })
-
 gulp.task('js', function () {
     return gulp.start(['js-libraries', 'js-app'])
 })
 // ----------------- //
+
 
 // WATCHERS
 gulp.task('watchFiles', function() {
@@ -96,8 +109,10 @@ gulp.task('watchFiles', function() {
     gulp.watch(`${paths.src}/**/*.html`, ['html'])
 })
 
+
 gulp.task('watch', ['watchFiles'])
 // ----------------- //
+
 
 // STATIC SERVER && HOT RELOAD
 gulp.task('serve', function() {
@@ -106,12 +121,12 @@ gulp.task('serve', function() {
             baseDir: `./${paths.build}`
         }
     })
-
     gulp.watch(`${paths.build}/*.html`).on('change', browserSync.reload)
     gulp.watch(`${paths.build}/css/*.css`).on('change', browserSync.reload)
     gulp.watch(`${paths.build}/js/*.js`).on('change', browserSync.reload)
 })
 // ----------------- //
+
 
 //DEFAULT task
 gulp.task('default', function() {
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,1 @@
 'use strict'
-
-const app = {}
-
-app.f = () => {
-    this.a = 'aaa'
-    console.log('aaa')
-}
